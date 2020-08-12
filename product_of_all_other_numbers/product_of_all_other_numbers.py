f1f4af45745b874f1cfbbf29066aa0b2c3e76cd4
Jordan <jordandevaney28@gmail.com>
2020-08-12 12:06:08 -0400

    I passed sliding window max, but I don't know how I did it

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,13 +4,20 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
+    # for loop 
+    for i in len(arr):
+        # need a nother for to keep track of the other pointer
+        if arr[i + 1]
+        prod *= i
+        print(prod)
+        # if arr i is the same as arr j, continue
+        # the index you are on times the values of every other index except the current index
+        # arr[0] = 1: 7*3*4 goes back into arr[0] for 84
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 7, 3, 4]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,9 +4,27 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    new_array = []
+    start = 0
+    # for loops with k number of steps
+    for i in range(start, len(nums)):
+        # print(f'Start is: {start} k is: {k}')
+        window = nums[start:k]
+        # print(f'sliding window arr is: {window}')
+        # set starting position to increment by one
+        start += 1
+        k += 1
+        max_value = max(window)
+        # push max value into new array
+        new_array.append(max_value)
 
-    pass
-
+        if (k > len(nums)):
+            break
+        
+        window = nums[start +1:k + 1]
+    return new_array
+        # increment sliding window
+        # sliding_window_max(nums, k + k)
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
