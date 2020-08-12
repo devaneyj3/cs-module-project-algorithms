153ff42ee71cb02e60a507309645d2d2d1201189
Jordan <jordandevaney28@gmail.com>
2020-08-11 22:53:05 -0400

    pass the moving-zeros problem. I need a little help on this one

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,11 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    return [noneZero for noneZero in arr if noneZero != 0] + [Zero for Zero in arr if Zero == 0]
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
