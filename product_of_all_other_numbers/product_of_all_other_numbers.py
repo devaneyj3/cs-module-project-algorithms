b51ebfc856b757d38b9a8f2841ce85274744ab9a
Jordan <jordandevaney28@gmail.com>
2020-08-13 17:07:00 -0400

    solved product_of_all_other_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,32 +2,38 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import math
+
+# TODO: I NEED HELP WITH THIS
+
+# def product_of_all_other_numbers(arr):
+#     new_arr = []
+    
+#     # loop from start of list
+#     for i in range(0, len(arr)):
+#         print(f'from start of lis {arr[i]}')
+#         i += 1
+#     for j in range(len(arr) -1):  
+#         print(arr[j])
+#         j -= 1
+#         # if arr i is the same as arr j, continue
+#         # the index you are on times the values of every other index except the current index
+#         # arr[0] = 1: 7*3*4 goes back into arr[0] for 84
+
 def product_of_all_other_numbers(arr):
-    new_arr = []
-    # while loop to loop through array
-    for i in range(0, len(arr)):
-        # need a nother for to keep track of the other pointer
-        # print(arr[i])
-        for j in range(0, len(arr)):       
-            print(f'{arr[i]}, {arr[j]}')
-            # # take value at current index and times it by every index but that one
-            if arr[i] != arr[j]:
-                print('in if')
-                arr[i] *= arr[j]
-                new_arr.append(arr[i])
-            # else:
-            #     return
-            # if arr[i + 1] < len(arr):
-            #     product *= arr[i + 1]
-            #     return product
-            # print(product)
-            j += 1
-        print(new_arr)
-        # if arr i is the same as arr j, continue
-        # the index you are on times the values of every other index except the current index
-        
-        
-        # arr[0] = 1: 7*3*4 goes back into arr[0] for 84
+    # prints [0,0,0,0] to have a temp new array
+    final_arr = [0] * len(arr)
+    # print(final_arr)
+    for i, elem in enumerate(arr):
+        j = i + 1
+        # print(f'counter j is {j} and i is {i}') 
+        new_arr = arr[:i] + arr[j:] if i != 0 else arr[j:]
+        # print(new_arr)
+        elem = math.prod(new_arr)
+        # print(elem)
+        final_arr[i] = elem
+    return final_arr
+
 
 
 if __name__ == '__main__':
