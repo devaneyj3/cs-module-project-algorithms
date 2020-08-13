566de3da6749a445cbcda1e390ce31e2fe201fc0
Jordan <jordandevaney28@gmail.com>
2020-08-13 15:32:53 -0400

    pass small test for cookie test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,17 +3,19 @@ Input: an integer
 Returns: an integer
 '''
 
-from itertools import permutations
 def eating_cookies(n):
     # if we eat less than 1 cookie
-    if n <= 1:
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
     
-    # go through all permutations of possible combinations
-    # for eating cookies
-    for i in permutations(range(n)):
-        print(i) 
+    
+    # go through all ways for eating cookies
     # how can ways can we eat 5 cookies 
+    else:
+        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+    return n
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
